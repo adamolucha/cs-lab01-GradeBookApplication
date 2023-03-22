30b9dcc7118fae34cb7422a4ce1554c68246481f
adamolucha <adam.olucha@microsoft.wsei.edu.pl>
2023-03-22 21:40:18 +0100

    Update StartingUserInterface's CreateCommand method

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,15 +34,35 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
-            Console.WriteLine("Created gradebook {0}.", name);
-            GradeBookUserInterface.CommandLoop(gradeBook);
+            //BaseGradeBook gradeBook = new BaseGradeBook(name);
+            //Console.WriteLine("Created gradebook {0}.", name);
+            //GradeBookUserInterface.CommandLoop(gradeBook);
+
+            var type = parts[2];
+            if (type == "standard")
+            {
+                StandardGradeBook standardGradebook = new StandardGradeBook(name);
+                Console.WriteLine("Created standard gradebook {0}.", name);
+                GradeBookUserInterface.CommandLoop(standardGradebook);
+            }
+            else if (type == "ranked")
+            {
+                RankedGradeBook rankedGradebook = new RankedGradeBook(name);
+                Console.WriteLine("Created ranked gradebook {0}.", name);
+                GradeBookUserInterface.CommandLoop(rankedGradebook);
+            }
+            else
+            {
+                Console.Write($"{parts[2]} is not a supported type of gradebook, please try again");
+            }
+
+   
         }
 
         public static void LoadCommand(string command)
